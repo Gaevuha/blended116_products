51ae9c11601e2944cc023ff86f6c68a8acedd895
Gaevuha <136448688+Gaevuha@users.noreply.github.com>
2025-05-12 12:20:07 +0000

    Deploying to gh-pages from @ Gaevuha/blended116_products@122f227f73599a10d7e93ffbf0416b90484d99c2 🚀

---
+++ wishlist.js
@@ -0,0 +1,2 @@
+import{u as t}from"./assets/render-function-h_8FbhVK.js";import"./assets/vendor-4yCzdkXl.js";document.addEventListener("DOMContentLoaded",()=>{t()});
+//# sourceMappingURL=wishlist.js.map
